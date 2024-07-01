d917c51cd7f80a408aa100f5798b640f417a4c1a
tutqq96 <tutqq96@gmail.com>
2024-07-01 23:39:40 +0700

    Init a gift for my love

--- src/App.jsx
+++ src/App.jsx
@@ -309,7 +309,7 @@ function App() {
           transform: "translate(-50%, -50%)",
           zIndex: 2
         }}>
-          <p>Hà Phương</p>
+          <p>Ngố Oishi</p>
         </div>
         <audio src="/VeDayNhe.mp3" autoPlay loop/>
         <canvas id="pinkboard">
